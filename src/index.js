fafe82e66448a6730276b2b2b8b6fd16be563fd0
Mauro Agostinelli <maufabagosgam@gmail.com>
2024-06-09 19:04:43 -0500

    nuevas ideas

--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
 import http from 'node:http'
 import { PORT } from './config.js'
-import {getUsuarios, index} from './controller.js'
- 
-const server = http.createServer(async(req,res)=>{
-    //const url = req.url
-    //const method = req.method
-    const {url, method} = req //esto es la abreviación de las 2 líneas de arriba
+import { getUsuarios, index, exportando, importando } from './controller.js'
 
-    if(method === 'GET'){
-        switch (url) {
-            case '/':
-                index(req,res)
-                break;
-            
-            case '/api/usuarios':
-                getUsuarios(req,res)
-                break;
-        
-            default:
-                res.end('Ruta no encontrada');
-                break;
-        }
-    }
+const server = http.createServer(async (req, res) => {
+  // const url = req.url
+  // const method = req.method
+  const { url, method } = req // esto es la abreviación de las 2 líneas de arriba
 
-    if(method === 'POST'){
-        //Codigo para rutas en POST
+  if (method === 'GET') {
+    switch (url) {
+      case '/':
+        index(req, res)
+        break
+
+      case '/api/usuarios':
+        getUsuarios(req, res)
+        break
+
+      case '/api/usuarios/export':
+        exportando(req, res)
+        break
+
+      case '/api/usuarios/import':
+        importando(req, res)
+        break
+
+      default:
+        res.end('Ruta no encontrada')
+        break
     }
+  }
+
+  if (method === 'POST') {
+    switch (url) {
+      case '/api/usuarios/import':
+        importando(req, res)
+        break
+    }
+  }
 })
 
-server.listen(PORT, ()=>console.log('Servidor ejecutandose'))
+server.listen(PORT, () => console.log('Servidor ejecutandose'))
